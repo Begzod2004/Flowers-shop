9827cc07df8af95737e8444f2461ed9973f20f19
Begzod2004 <mirzaqulovbekmurod@gmail.com>
2022-12-20 14:39:37 +0500

    new -----------2

--- config/settings.py
+++ config/settings.py
@@ -186,8 +186,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 # EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 # EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-EMAIL_HOST_USER = 'begzodmirzaqulov71@gmail.com'
-EMAIL_HOST_PASSWORD = 'ajrkyssdbpxcgigs'
+EMAIL_HOST_USER = 'ozodbekyakhshiboevman@gmail.com'
+EMAIL_HOST_PASSWORD = 'imksaktvkxxebnii'
 
 
 SIMPLE_JWT = {
